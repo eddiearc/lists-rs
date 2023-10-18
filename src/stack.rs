c6a7eb06a85c23e095cd347812e01a4a0972e3e4
eddie <eddievim@foxmail.com>
2023-10-19 00:41:06 +0800

    stack implement

--- src/stack.rs
+++ src/stack.rs
@@ -1,22 +1,50 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{rc::Rc};
 
-pub struct MyStack<T> {
-    cur_node: Option<RefCell<Rc<Node<T>>>>,
+pub struct MyStack<T: Clone> {
+    tail: Option<Rc<Node<T>>>,
 }
 
-impl<T> MyStack<T> {
+impl<T: Clone> MyStack<T> {
+    pub fn new() -> MyStack<T> {
+        return MyStack { tail: None }
+    }
     pub fn push(&mut self, val: T) {
-        
+        match self.tail {
+            None => {
+                let node = Rc::new(Node::new(val));
+                self.tail = Some(node);
+            },
+            Some(ref mut node) => {
+                let mut new_node = Node::new(val);
+                new_node.next = Some(Rc::clone(node));
+                self.tail = Some(Rc::new(new_node));
+            },
+        }
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        None
+        if self.tail.is_none() {
+            return None;
+        }
+        let binding = self.tail.take();
+        let node = binding.as_ref().unwrap();
+        self.tail = match &node.next {
+            None => None,
+            Some(node) => Some(Rc::clone(&node)),
+        };
+        
+        return Some(node.val.clone());
     }
 }
 
 // Node for MyStack
-struct Node<T> {
+struct Node<T: Clone> {
     val: T,
-    prev: Option<RefCell<Rc<Node<T>>>>,
-    next: Option<RefCell<Rc<Node<T>>>>,
+    next: Option<Rc<Node<T>>>,
+}
+
+impl<T: Clone> Node<T> {
+    fn new(val: T) -> Node<T> {
+        Node {val: val, next: None}
+    }
 }
